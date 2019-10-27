baadd07e5c6aa63cc5337cc7ce8312e6d2d707a8
Mauro Pinheiro <mauro.pinheiro@acad.ifma.edu.br>
2019-10-27 14:25:03 -0300

    Aula 14 Menu Lateral Font Awsome

--- resources/views/templates/master.blade.php
+++ resources/views/templates/master.blade.php
@@ -4,12 +4,14 @@
         <meta charset="UTF-8">
         <title>Investindo</title>
         <link rel='stylesheet' href="{{asset('css/stylesheet.css')}}">
+        <link href="https://fonts.googleapis.com/css?family=Fredoka+One&display=swap" rel="stylesheet">
+        <script src="https://kit.fontawesome.com/598d73746c.js" crossorigin="anonymous"></script>
         @yield('vss-view')
     </head>
 
     <body>
+        @include('templates.menu-lateral')
         @yield('conteudo-view')
-
         @yield('js-view')
     </body>
 </html>
---
+++ resources/views/templates/menu-lateral.blade.php
@@ -0,0 +1,22 @@
+<nav id="principal">
+    <ul>
+        <li>
+            <a href="">
+                <i class="fas fa-address-book"></i>
+                <h3>Usuarios</h3>
+            </a>
+        </li>
+        <li>
+            <a href="">
+                <i class="fas fa-building"></i>
+                <h3>Instituicões</h3>
+            </a>
+        </li>
+        <li>
+            <a href="">
+                    <i class="fas fa-users"></i>
+                <h3>Grupos</h3>
+            </a>
+        </li>
+    </ul>
+</nav>
